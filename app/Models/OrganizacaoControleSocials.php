2a686ecf0610467e6742faa1d56a02a64f0f1acc
GuilhermeGz <guilherme.dna06@gmail.com>
2022-10-27 16:54:56 -0300

    Criado arquivo de migração e model para Organização de Controle Social

---
+++ app/Models/OrganizacaoControleSocials.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class OrganizacaoControleSocials extends Model
+{
+    use HasFactory;
+}
