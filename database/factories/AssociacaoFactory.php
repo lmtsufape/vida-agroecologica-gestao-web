8d2e8e65414a9c4f235cf8235d9283d9251d5553
Edgarvital <edgar.tecnico@hotmail.com>
2022-10-27 16:54:12 -0300

    Factory e Seeder de Associação

---
+++ database/factories/AssociacaoFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class AssociacaoFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'codigo' => '01010101',
+            'nome' => 'Associação 1',
+            'user_id' => 2
+        ];
+    }
+}
