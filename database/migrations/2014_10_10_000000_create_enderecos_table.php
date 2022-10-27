11f938a8a3368729220e55a52e0de8e7e947bf80
GuilhermeGz <guilherme.dna06@gmail.com>
2022-10-27 16:41:43 -0300

    Criada arquivo de migração e model para Endereço

---
+++ database/migrations/2014_10_10_000000_create_enderecos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('enderecos', function (Blueprint $table) {
+            $table->id();
+            $table->string('pais');
+            $table->string('cidade');
+            $table->string('uf');
+            $table->string('rua');
+            $table->string('bairro');
+            $table->string('numero');
+            $table->string('cep');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('enderecos');
+    }
+};
