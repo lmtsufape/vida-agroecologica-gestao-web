fefa89baf644efc52f958449e79946ba37602ec5
Edgarvital <edgar.tecnico@hotmail.com>
2022-11-02 16:36:41 -0300

    Atualização das rotas de usuario e associação na parte da api

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,12 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::prefix('auth')->group(function (){
-   Route::post('login', [App\Http\Controllers\Auth\Api\LoginController::class,'login']);
-});
+Route::post('login', [App\Http\Controllers\Auth\Api\LoginController::class, 'login']);
+Route::post('cadastro', [App\Http\Controllers\Auth\Api\UserController::class, 'store']);
+Route::post('atualizar/usuario', [App\Http\Controllers\Auth\Api\UserController::class, 'update']);
+Route::get('users', [\App\Http\Controllers\Auth\Api\UserController::class, 'index']);
+
+Route::post('cadastrar/associacao', [\App\Http\Controllers\Auth\Api\AssociacaoController::class, 'store']);
+Route::post('atualizar/associacao', [\App\Http\Controllers\Auth\Api\AssociacaoController::class, 'update']);
+Route::get('associacoes', [\App\Http\Controllers\Auth\Api\AssociacaoController::class, 'index']);
+
