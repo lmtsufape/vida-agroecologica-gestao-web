665be4715abef8b90ed10ebc8672a0a60ed6dee2
GuilhermeGz <guilherme.dna06@gmail.com>
2022-11-02 15:29:11 -0300

    Criação de login base para a api

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('auth')->group(function (){
+   Route::post('login', [App\Http\Controllers\Auth\Api\LoginController::class,'login']);
+});
