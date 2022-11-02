08e517fc17c8243f981312f4dc29210250814fa3
GuilhermeGz <guilherme.dna06@gmail.com>
2022-11-02 16:42:20 -0300

    Adicionada funções da api para Organizacao de Controle Social

--- routes/api.php
+++ routes/api.php
@@ -21,3 +21,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::prefix('auth')->group(function (){
    Route::post('login', [App\Http\Controllers\Auth\Api\LoginController::class,'login']);
 });
+
+// OCS
+Route::post('/organizacaoControleSocial/store', [App\Http\Controllers\Api\OrganizacaoControleSocialController::class,'store']);
+Route::post('/organizacaoControleSocial/update', [App\Http\Controllers\Api\OrganizacaoControleSocialController::class,'update']);
+Route::get('/associacao/{associacao_id}/organizacaoControleSocial', [App\Http\Controllers\Api\OrganizacaoControleSocialController::class,'index']);
+
